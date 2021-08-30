d35d0a8c9c34e8c6a4671cca2dbabb05aeedf41a
stepin105057 <69064298+stepin105057@users.noreply.github.com>
2021-08-30 13:01:08 +0530

    Delete Hooks.js

--- cypress/integration/DanfossTestAutomation/HighAlarm/Hooks.js
+++
@@ -1,7 +0,0 @@
-/*before(() => {
-    cy.visit('https://118.185.187.242:6027')
-    cy.wait(2000)
-    cy.get('#user').should('be.visible').type('Supervisor')//username
-    cy.get('#pwd').type('S@danfoss123')//password
-    cy.get('form.ng-dirty > .btn').click()//signin
-})*/
